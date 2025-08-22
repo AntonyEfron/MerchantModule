ad9346ad927986a9cf480355561d9b3ff4b119ea
Antonyefron <antonyefron007@gmail.com>
2025-08-22 18:12:34 +0530

    jdhjehdjeh

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.0.104:5000/api', // no trailing slash
+  baseURL: 'http://192.168.29.18:5000/api', // no trailing slash
   timeout: 10000,
 });
 
